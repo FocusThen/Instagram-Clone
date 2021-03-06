547c1b479dfb23e60bf7e5e1569c2449b61e917b
M. Mücahit TEZCAN <mmucahit.tezcan@gmail.com>
2021-03-07 02:46:24 +0300

    creating Post sections and Time Line

--- src/components/TheTimeline/TheTimeLine.tsx
+++ src/components/TheTimeline/TheTimeLine.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import Skeleton from 'react-loading-skeleton'
+import { useFollowedUsersPhotos } from '@app/hooks/useFollowedUsersPhotos'
 
 export default function TheTimeLine() {
-  return <div></div>
+  const { photos } = useFollowedUsersPhotos()
+  return (
+    <div className="container col-span-2">
+      {!photos ? (
+        <Skeleton count={4} width={640} height={500} className="mb-5" />
+      ) : photos.length > 0 ? (
+        photos.map((content) => <p key={content.docId}>{content.username}</p>)
+      ) : (
+        <p className="text-center text-2xl">Follow people to see photos!</p>
+      )}
+    </div>
+  )
 }
