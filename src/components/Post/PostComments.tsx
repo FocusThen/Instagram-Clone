b36758fa9c6b4620cdd337200b9085d449e88cec
M. Mücahit TEZCAN <mmucahit.tezcan@gmail.com>
2021-03-08 02:32:44 +0300

    Post component & Header and Comments section 🐱‍💻

---
+++ src/components/Post/PostComments.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { formatDistance } from 'date-fns'
+import { Link } from 'react-router-dom'
+
+import PostAddComment from '@app/components/Post/PostAddComment'
+
+export type PostCommentsProps = {
+  docId: string
+  comments: {
+    comment: string
+    displayName: string | null
+  }[]
+  posted: number
+  commentInput: React.RefObject<HTMLDivElement>
+}
+
+export default function PostComments({
+  docId,
+  comments: allComments,
+  posted,
+  commentInput,
+}: PostCommentsProps) {
+  const [comments, setComments] = useState(allComments)
+
+  return (
+    <>
+      <div className="p-4 pt-1 pb-4">
+        {comments.length >= 3 && (
+          <p className="text-sm text-gray-500 mb-1 cursor-pointer">
+            View All {comments.length} comments
+          </p>
+        )}
+        {comments.slice(0, 3).map((item) => (
+          <p key={`${item.comment}-${item.displayName}`} className="mb-1">
+            <Link to={`/p/${item.displayName}`}>
+              <span className="mr-1 font-bold">{item.displayName}</span>
+            </Link>
+            <span>{item.comment}</span>
+          </p>
+        ))}
+        <p className="text-gray-500 text-xs mt-2">
+          {formatDistance(posted, new Date())} age
+        </p>
+      </div>
+      <PostAddComment
+        docId={docId}
+        comments={comments}
+        setComments={setComments}
+        commentInput={commentInput}
+      />
+    </>
+  )
+}
--- src/components/Post/PostHeader.tsx
+++ src/components/Post/PostHeader.tsx
@@ -1,5 +1,24 @@
 import React from 'react'
 
-export default function PostHeader(): JSX.Element {
-  return <div></div>
+import { Link } from 'react-router-dom'
+
+export type PostHeaderProps = {
+  username: string
+}
+
+export default function PostHeader({ username }: PostHeaderProps): JSX.Element {
+  return (
+    <div className="flex border-b h-4 p-4 py-8">
+      <div className="flex items-center">
+        <Link to={`/p/${username}`} className="flex items-center">
+          <img
+            className="rounded-full h-8 w-8 flex mr-3"
+            src={`/assets/images/avatars/${username}.jpg`}
+            alt={`${username} Profile Picture`}
+          />
+          <p className="font-bold">{username}</p>
+        </Link>
+      </div>
+    </div>
+  )
 }
